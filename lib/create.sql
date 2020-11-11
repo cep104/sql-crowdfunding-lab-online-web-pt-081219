b3dc40743b9d4f466c206e8a713d77689107832a
Candice Peters <51180851+cep104@users.noreply.github.com>
2020-11-11 17:44:22 -0600

    Update create.sql

--- lib/create.sql
+++ lib/create.sql
@@ -18,3 +18,8 @@ amount INTEGER,
 user_id INTEGER,
 project_id INTEGER
 );
+
+-- #makes the database that is going to create these tables
+-- to run data from terminal sqlite3 name_of_database.rb < name_of_file.rb(ex. create.sql)
+-- in terminal type ls and see your database
+-- to open in db generater type open name_of_database.rb
